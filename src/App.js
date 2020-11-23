4d84a4c35593f610d44098f4e4f0164c5003f0f1
Paruyr Avetisyan <paruyr.avetisyan@softconstruct.local>
2020-11-23 14:55:09 +0400

    creating counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Animal from './animals/animals';
+import Counter from './Counter/counter';
 
 function App() {
   return (
     <div className="App">
-      <Animal animals={['rex', 'murka', 'chao', 'flyuk', 'kity', 'bety']}/>
+      {/* <Animal animals={['rex', 'murka', 'chao', 'flyuk', 'kity', 'bety']}/> */}
+      <Counter />
     </div>
   );
 }
---
+++ src/Counter/counter.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './counter.css';
+
+
+export default class Counter extends Component {
+
+    constructor(){
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    state = {
+        count: 0
+    }
+
+    handleClick(type){
+        return () => {
+            switch(type){
+                case 'inc':
+                    this.setState({ count: this.state.count + 1 });
+                    break;
+                case 'dec':
+                    if(!this.state.count){
+                        return;
+                    }
+                    this.setState({ count: this.state.count - 1 });
+                    break;
+                case 'reset':
+                    this.setState({ count: 0 });
+                    break;
+
+            }
+        }
+    }
+
+    render(){
+        return (
+            <div className='wrap'>
+                <h1>{this.state.count}</h1>
+                <div>
+                    <button className={`decrement ${this.state.count === 0 ? 'disabled' : ''}`} onClick={this.handleClick('dec')} >decrement</button>
+                    <button className='increment' onClick={this.handleClick('inc')}>increment</button>
+                    <button className='reset' onClick={this.handleClick('reset')}>reset</button>
+                </div>
+            </div>
+        )
+    }
+}
